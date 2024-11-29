ae939d89e9fb73cbfd1a47e5dc2567b35187ece5
mayankpatel-in <mayankpatel1483@gmail.com>
2024-11-29 14:13:10 +0530

    apiserver word added on slug

--- basuri_api/urls.py
+++ basuri_api/urls.py
@@ -5,16 +5,16 @@ from django.conf.urls.static import static
 
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
-    path('my_admin/', include('admin_panel.urls')),
-    path('accounts/', include('accounts.urls')),
-    path('address/', include('address.urls')),
-    path('product/', include('product.urls')),
-    path('utils/', include('utils.urls')),
-    path('cart/', include('cart.urls')),
-    path('order/', include('order.urls')),
-    path('checkout/', include('checkout.urls')),
-    path('coupon/', include('discount.urls')),
-    path('payment/', include('payment.urls')),
+    path('apiserver/admin/', admin.site.urls),
+    path('apiserver/my_admin/', include('admin_panel.urls')),
+    path('apiserver/accounts/', include('accounts.urls')),
+    path('apiserver/address/', include('address.urls')),
+    path('apiserver/product/', include('product.urls')),
+    path('apiserver/utils/', include('utils.urls')),
+    path('apiserver/cart/', include('cart.urls')),
+    path('apiserver/order/', include('order.urls')),
+    path('apiserver/checkout/', include('checkout.urls')),
+    path('apiserver/coupon/', include('discount.urls')),
+    path('apiserver/payment/', include('payment.urls')),
 
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
